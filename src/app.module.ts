d322c3f43ccb0eb73b3590b7401247115e2a4862
Renat <jeingo20@gmail.com>
2023-01-31 15:06:23 +0300

    add connect to mongodb(mongoose) and configuration(.env)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { settings } from './settings/settings';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(settings.MONGO_URL, {
+      dbName: settings.DB_NAME,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/settings/settings.ts
@@ -0,0 +1,5 @@
+export const settings = {
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
+  DB_NAME: process.env.DB_NAME || 'service',
+  PORT: process.env.PORT || 5000,
+};
