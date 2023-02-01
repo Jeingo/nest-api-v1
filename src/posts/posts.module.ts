de0b8b63dbc52e26ad3b3ae35ea888537412e6c1
Renat <jeingo20@gmail.com>
2023-02-01 15:19:48 +0300

    enable postqueryrepo postrepo and Model to module post

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { PostsQueryRepository } from './posts.query.repository';
+import { PostsRepository } from './posts.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Post, PostSchema } from './entities/post.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }])
+  ],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService, PostsQueryRepository, PostsRepository]
 })
 export class PostsModule {}
