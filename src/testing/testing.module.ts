b946d5853c7320c67020ec4768f55f09cf5a11d1
Renat <jeingo20@gmail.com>
2023-02-02 17:38:22 +0300

    fix deleting models

--- src/testing/testing.module.ts
+++ src/testing/testing.module.ts
@@ -3,10 +3,18 @@ import { TestingService } from './testing.service';
 import { TestingController } from './testing.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from '../blogs/entities/blog.entity';
+import { Post, PostSchema } from '../posts/entities/post.entity';
+import { User, UserSchema } from '../users/entities/user.entity';
+import { Comment, CommentSchema } from '../comments/entities/comment.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }])
+    MongooseModule.forFeature([
+      { name: Blog.name, schema: BlogSchema },
+      { name: Post.name, schema: PostSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Comment.name, schema: CommentSchema }
+    ])
   ],
   controllers: [TestingController],
   providers: [TestingService]
--- src/testing/testing.service.ts
+++ src/testing/testing.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Blog, IBlogModel } from '../blogs/entities/blog.entity';
+import { IUserModel, User } from '../users/entities/user.entity';
+import { IPostModel, Post } from '../posts/entities/post.entity';
+import { Comment, ICommentModel } from '../comments/entities/comment.entity';
 
 @Injectable()
 export class TestingService {
-  constructor(@InjectModel(Blog.name) private blogsModel: IBlogModel) {}
+  constructor(
+    @InjectModel(Blog.name) private blogsModel: IBlogModel,
+    @InjectModel(User.name) private usersModel: IUserModel,
+    @InjectModel(Post.name) private postsModel: IPostModel,
+    @InjectModel(Comment.name) private commentsModel: ICommentModel
+  ) {}
 
   async removeAll() {
     await this.blogsModel.deleteMany({});
+    await this.usersModel.deleteMany({});
+    await this.postsModel.deleteMany({});
+    await this.commentsModel.deleteMany({});
   }
 }
