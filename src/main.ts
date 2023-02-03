c05079265d4c9a1d075f671ba18a086a35bf4929
Renat <jeingo20@gmail.com>
2023-02-03 20:53:00 +0300

    add exception filter and make custom errors object for 400 exception

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { IConfigType } from './configuration/configuration';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './helper/expceptionFilter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<IConfigType>);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
