371ddc1337a081cf16bcf83b7cc2d49d93241afd
Renat <jeingo20@gmail.com>
2023-02-07 00:51:41 +0300

    fix 5

--- src/blogs/dto/input.create.blog.dto.ts
+++ src/blogs/dto/input.create.blog.dto.ts
@@ -1,5 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Trim } from '../../helper/pipes/validation.pipe';
+
 export class InputCreateBlogDto {
+  @IsString()
+  @IsNotEmpty()
+  @Trim()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Trim()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Trim()
   websiteUrl: string;
 }
