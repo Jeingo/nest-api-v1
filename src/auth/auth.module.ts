5132c8c733a5984d27a664c0fc7d13d3579c5124
Renat <jeingo20@gmail.com>
2023-02-06 11:33:26 +0300

    delete passport

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/entities/user.entity';
+import { UsersRepository } from '../users/users.repository';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UsersRepository]
+})
+export class AuthModule {}
