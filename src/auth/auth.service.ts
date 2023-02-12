817a58d05f48c3fae9e6bfd330e4bf0f16cecbf8
Renat <jeingo20@gmail.com>
2023-02-13 01:07:29 +0300

    empty auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { UsersRepository } from '../users/users.repository';
-import { Token, RefreshTokenPayloadType } from '../adapters/jwt/types/jwt.type';
-import { JwtAdapter } from '../adapters/jwt/jwt.service';
-import { SessionsService } from '../sessions/sessions.service';
 
 @Injectable()
-export class AuthService {
-  constructor(
-    private readonly usersRepository: UsersRepository,
-    private readonly jwtAdapter: JwtAdapter,
-    private readonly sessionsService: SessionsService
-  ) {}
-
-  async checkAuthorizationAndGetPayload(
-    refreshToken: Token
-  ): Promise<RefreshTokenPayloadType | false> {
-    const result = this.jwtAdapter.checkExpirationRefreshToken(refreshToken);
-    if (!result) return false;
-    const payload = this.jwtAdapter.getRefreshTokenPayload(refreshToken);
-    const statusSession = await this.sessionsService.isActiveSession(
-      payload.deviceId
-    );
-    if (!statusSession) return false;
-    return payload;
-  }
-}
+export class AuthService {}
--- src/auth/guards/cookie.guard.ts
+++ src/auth/guards/cookie.guard.ts
@@ -5,21 +5,41 @@ import {
   UnauthorizedException
 } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import {
+  RefreshTokenPayloadType,
+  Token
+} from '../../adapters/jwt/types/jwt.type';
+import { JwtAdapter } from '../../adapters/jwt/jwt.service';
+import { SessionsService } from '../../sessions/sessions.service';
 
 @Injectable()
 export class CookieGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtAdapter: JwtAdapter,
+    private readonly sessionsService: SessionsService
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const refreshToken = request.cookies?.refreshToken;
-    const payload = await this.authService.checkAuthorizationAndGetPayload(
-      refreshToken
-    );
+    const payload = await this.checkAuthorizationAndGetPayload(refreshToken);
     if (!payload) {
       throw new UnauthorizedException();
     }
     request.payload = payload;
     return true;
+  }
+  private async checkAuthorizationAndGetPayload(
+    refreshToken: Token
+  ): Promise<RefreshTokenPayloadType | false> {
+    const result = this.jwtAdapter.checkExpirationRefreshToken(refreshToken);
+    if (!result) return false;
+    const payload = this.jwtAdapter.getRefreshTokenPayload(refreshToken);
+    const statusSession = await this.sessionsService.isActiveSession(
+      payload.deviceId
+    );
+    if (!statusSession) return false;
+    return payload;
   }
 }
