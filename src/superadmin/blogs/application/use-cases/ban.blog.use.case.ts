28ebb374ae585ace23bd910f479dcc5a1bec344a
Renat <jeingo20@gmail.com>
2023-02-20 21:11:09 +0300

    fix bug

--- src/superadmin/blogs/application/use-cases/ban.blog.use.case.ts
+++ src/superadmin/blogs/application/use-cases/ban.blog.use.case.ts
@@ -2,6 +2,7 @@ import { CommandHandler } from '@nestjs/cqrs';
 import { DbId } from '../../../../global-types/global.types';
 import { BlogsRepository } from '../../../../blogs/infrastructure/blogs.repository';
 import { InputBanBlogDto } from '../../api/dto/input.ban.blog.dto';
+import { NotFoundException } from '@nestjs/common';
 
 export class BanBlogCommand {
   constructor(public blogId: DbId, public banBlogDto: InputBanBlogDto) {}
@@ -13,6 +14,7 @@ export class BanBlogUseCase {
 
   async execute(command: BanBlogCommand): Promise<boolean> {
     const blog = await this.blogsRepository.getById(command.blogId);
+    if (!blog) throw new NotFoundException();
     blog.banBlog(command.banBlogDto.isBanned);
     await this.blogsRepository.save(blog);
     return true;
