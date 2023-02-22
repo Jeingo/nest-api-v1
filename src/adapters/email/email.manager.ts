c09038ff7a8f1009897a7b418404d87b8e68f4a7
Renat <jeingo20@gmail.com>
2023-02-22 15:02:19 +0300

    refactoring email adapter

--- src/adapters/email/email.manager.ts
+++ src/adapters/email/email.manager.ts
@@ -11,7 +11,7 @@ export class EmailManager {
       from: '"Backend-09" <backend.jeingo@gmail.com>',
       to: user.email,
       subject: 'Registration confirmation',
-      html: this._registrationConfirmationMessage(
+      html: this.registrationConfirmationMessage(
         user.emailConfirmation.confirmationCode
       )
     };
@@ -24,13 +24,13 @@ export class EmailManager {
       from: '"Backend-09" <backend.jeingo@gmail.com>',
       to: user.email,
       subject: 'Password recovery confirmation',
-      html: this._passwordRecoveryConfirmationMessage(
+      html: this.passwordRecoveryConfirmationMessage(
         user.passwordRecoveryConfirmation.passwordRecoveryCode
       )
     };
     await this.emailService.sendEmail(emailForm);
   }
-  private _registrationConfirmationMessage(code: string): string {
+  private registrationConfirmationMessage(code: string): string {
     return `
         <h1>Thank for your registration</h1>
             <p>To finish registration please follow the link below:
@@ -38,7 +38,7 @@ export class EmailManager {
             </p>
            `;
   }
-  private _passwordRecoveryConfirmationMessage(code: string): string {
+  private passwordRecoveryConfirmationMessage(code: string): string {
     return `
         <h1>Password recovery</h1>
             <p>To finish password recovery please follow the link below:
